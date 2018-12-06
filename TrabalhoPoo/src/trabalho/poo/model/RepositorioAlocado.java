917f78293e67b0047a6fa30b48db906bb861b8ad
Guilherme Sampaio <gui85238523@gmail.com>
2018-12-06 00:58:57 -0200

    sql

--- TrabalhoPoo/src/trabalho/poo/model/RepositorioAlocado.java
+++ TrabalhoPoo/src/trabalho/poo/model/RepositorioAlocado.java
@@ -1,9 +1,15 @@
+package trabalho.poo.model;
+
 import java.util.*;
+
+import trabalho.poo.controller.Clientes;
+import trabalho.poo.controller.Funcionario;
+
 import java.sql.*;
 
 public class RepositorioAlocado
 {	public static int incluir (Connection conn, int cod_func, int cod_ativ, String funcao) 
-		throws Excecao
+		throws SQLException
 	{		
 		try
 		{	PreparedStatement pstmt = conn.prepareStatement 
@@ -40,11 +46,11 @@ public class RepositorioAlocado
 //			} 
 		}
 
-		return pk;
+		return 0;
 	}
 
 	public static boolean alterar (Connection conn, int cod_func, int cod_ativ, String funcao) 
-		throws Excecao
+		throws SQLException
 	{		
 		int n = 0;
 
@@ -52,7 +58,7 @@ public class RepositorioAlocado
 		{	PreparedStatement pstmt = conn.prepareStatement 
 				("UPDATE ALOCADO SET FUNCAO = ? WHERE FUNC_COD = ? AND ATIV_COD = ?");
 				
-			pstmt.setString	(1, nome);	
+			pstmt.setString	(1, funcao);	
 			pstmt.setInt	(2, cod_func);	
 			pstmt.setInt	(3, cod_ativ);
 				
@@ -61,24 +67,25 @@ public class RepositorioAlocado
 		}
 		catch(SQLException e)
 		{	
-			if(e.getSQLState().equals(Constantes.VIOLACAO_INDICE_UNICO))
-			{	
-				if (e.getMessage().toUpperCase().indexOf("ALOCADO_FUNC_COD_FK") != -1)
-				{	throw new Excecao(1, "Codigo de Funcionário duplicado.");
-				}
-				else if (e.getMessage().toUpperCase().indexOf("ALOCADO_ATIV_COD_FK") != -1)
-				{	throw new Excecao(2, "Codigo de Atividade duplicado.");
-				}
-				else
-				{	System.out.println('\n' + "Erro na alteração de uma Alocação.");
-					e.printStackTrace();
-					System.exit(1);			
-				}
-			}
-			else
-			{	e.printStackTrace();
-				System.exit(1);
-			} 
+//			if(e.getSQLState().equals(Constantes.VIOLACAO_INDICE_UNICO))
+//			{	
+//				if (e.getMessage().toUpperCase().indexOf("ALOCADO_FUNC_COD_FK") != -1)
+//				{	throw new Excecao(1, "Codigo de Funcionário duplicado.");
+//				}
+//				else if (e.getMessage().toUpperCase().indexOf("ALOCADO_ATIV_COD_FK") != -1)
+//				{	throw new Excecao(2, "Codigo de Atividade duplicado.");
+//				}
+//				else
+//				{	System.out.println('\n' + "Erro na alteração de uma Alocação.");
+//					e.printStackTrace();
+//					System.exit(1);			
+//				}
+//			}
+//			else
+//			{	
+			e.printStackTrace();
+//				System.exit(1);
+//			} 
 		}
 
 		return n == 1;
@@ -110,8 +117,8 @@ public class RepositorioAlocado
 		return n == 1;
 	}
 
-	public static Cliente recuperaUmaAlocacao(Connection conn, int cod_func, int cod_ativ)
-	{	Empregado e = null;
+	public static Funcionario recuperaUmaAlocacao(Connection conn, int cod_func, int cod_ativ)
+	{	Funcionario e = null;
 
 		try
 		{	PreparedStatement pstmt = conn.prepareStatement
@@ -122,7 +129,7 @@ public class RepositorioAlocado
 			pstmt.setInt(2, cod_ativ);
 			ResultSet rs = pstmt.executeQuery();
 	 		if (rs.next())
-	 		{	e = new Empregado(rs.getInt("FUNC_COD"),
+	 		{	e = new Funcionario(rs.getInt("FUNC_COD"),
 					              rs.getInt("ATIV_COD"), 
 					              rs.getString("FUNCAO"));
 	 		}
